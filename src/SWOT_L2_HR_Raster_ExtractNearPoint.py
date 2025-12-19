53584cd4d69a533c3c5ca39336c93619ed704613
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2025-12-19 14:50:20 -0800

    Update SWOT_L2_HR_Raster_ExtractNearPoint.py

--- src/SWOT_L2_HR_Raster_ExtractNearPoint.py
+++ src/SWOT_L2_HR_Raster_ExtractNearPoint.py
@@ -51,19 +51,20 @@ if len(sys.argv) < 2:
 ##############################################################################
 ##############################################################################
 ## Set Directory
-# base_dir = Path(os.path.realpath(__file__)).parent.parent.parent
-base_dir = Path(os.path.dirname(os.getcwd())).parent
-
+base_dir = Path(os.path.realpath(__file__)).parent.parent
+# base_dir = Path(os.path.dirname(os.getcwd())).parent
+print(base_dir)
 
 ##############################################################################
 ##############################################################################
 ## Get bounding information from CSV. If you don't have this file or you're working on a new AOI, you can define boundaries below
 try:
-    bounding_LUT = pd.read_csv(base_dir / 'config'/ 'aoi.csv')
-    aois = list(bounding_LUT['aoi'])
+    bounding_LUT = pd.read_csv(base_dir / 'point_template.csv')
+    aois = list(np.unique(bounding_LUT['aoi']))
 except:
     bounding_LUT = pd.DataFrame([['default',np.nan,np.nan,np.nan,np.nan,10,15,-2,2,0,1]],columns=['aoi','minx','miny','maxx','maxy','minssh','maxssh','minwse','maxwse','mindac','maxdac'])
     aois = []
+
     
 if args.interactive:
     aoi = input('which AOI: %s'%(aois))
@@ -74,28 +75,28 @@ else:
     types = args.types
     search_radius_hr = float(args.search_radius_hr)
  
-## Get bounding areas of AOI
-if aoi not in aois:
-    LUT =  bounding_LUT[bounding_LUT['aoi']=='default'].reset_index()
-    print('what bounding box (lat/lon in decimal degrees)')
-    LUT['minx'][0] = input('xmin: ')
-    LUT['miny'][0] = input('ymin: ')
-    LUT['maxx'][0] = input('xmax: ')
-    LUT['maxy'][0] = input('ymax: ')
-else:
-    LUT =  bounding_LUT[bounding_LUT['aoi']==aoi].reset_index()
+# ## Get bounding areas of AOI
+# if aoi not in aois:
+#     LUT =  bounding_LUT[bounding_LUT['aoi']=='default'].reset_index()
+#     print('what bounding box (lat/lon in decimal degrees)')
+#     LUT['minx'][0] = input('xmin: ')
+#     LUT['miny'][0] = input('ymin: ')
+#     LUT['maxx'][0] = input('xmax: ')
+#     LUT['maxy'][0] = input('ymax: ')
+# else:
+#     LUT =  bounding_LUT[bounding_LUT['aoi']==aoi].reset_index()
 
-area = [LUT['minx'][0],LUT['miny'][0],LUT['maxx'][0],LUT['maxy'][0]]
+# area = [LUT['minx'][0],LUT['miny'][0],LUT['maxx'][0],LUT['maxy'][0]]
 
-## Correct any negative E-W coordinates
-if area[0] <0: 
-    area2 = [360 + area[0],area[1],360+area[2],area[3]]
-else:
-    area2 = [  area[0],area[1],area[2],area[3]]
-    area = [  area[0]-360,area[1],area[2]-360,area[3]]
+# ## Correct any negative E-W coordinates
+# if area[0] <0: 
+#     area2 = [360 + area[0],area[1],360+area[2],area[3]]
+# else:
+#     area2 = [  area[0],area[1],area[2],area[3]]
+#     area = [  area[0]-360,area[1],area[2]-360,area[3]]
 
-print('Crop to Area: %s-%s' %(np.round(area[1],1),np.round(area[3],1)))
-print('Crop to Area: %s-%s' %(np.round(area2[1],1),np.round(area2[3],1)))
+# print('Crop to Area: %s-%s' %(np.round(area[1],1),np.round(area[3],1)))
+# print('Crop to Area: %s-%s' %(np.round(area2[1],1),np.round(area2[3],1)))
 
 ##############################################################################
 ##############################################################################
@@ -142,30 +143,17 @@ variables_to_save = list(ds.keys())
 
 
 
-##### Get Gauge data
-gauge_folder = base_dir / "Validation" / 'gauges' / aoi #'stlawrence'
-if aoi == 'louisiana':
-    gauge_folder = '/projects/loac_hydro/alchrist/CRMS/'
-if types == 'profiles':
-    coords = pd.read_csv(base_dir / "Validation" / 'gauges' / 'profiles_SWOT_all.csv')
-if types == 'points':
-    coords = pd.read_csv(base_dir / "Validation" / 'gauges' / 'gauges_SWOT_all.csv')
-coords = coords[coords['aoi']==aoi].reset_index()
-passes = [ast.literal_eval(i) for i in coords['pass']]
-scenes = [ast.literal_eval(i) for i in coords['scene']]
-tiles = [ast.literal_eval(i) for i in coords['tile']]
-allpasses = [x for xs in passes for x in xs]
-allpasses = np.unique(allpasses)
-alltiles = [x for xs in tiles for x in xs]
-alltiles = np.unique(alltiles)
+##### Get Point Information
+output_dir = base_dir / 'Examples' / aoi
+Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-g_names = coords['gaugename']
+coords = pd.read_csv(base_dir / 'point_template.csv')
+coords = coords[coords['aoi']==aoi].reset_index()
+
+g_names = coords['name']
 gs = np.arange(0,len(g_names))
 g_lons = coords['longitude'].astype(float)
 g_lats = coords['latitude'].astype(float)
-g_firstdates = coords['firstdate']
-g_lastdates = coords['lastdate']
-g_files = coords['filename']
 
 
 ##############################################################################
@@ -197,9 +185,9 @@ swot_products = [l2hr_prefix+ '_' + i for i in ['100m'] ]
 for g in gs:
     ## Gauge Name
     gauge_name = g_names[g]
-    print('\n\n\n\n[%s-%s]: %s' %(gauge_name,g, g_files[g]))
+    print('\n\n\n\n[%s-%s]' %(gauge_name,g))
     
-    gauge_info = coords[coords['gaugename']==gauge_name].reset_index()
+    gauge_info = coords[coords['name']==gauge_name].reset_index()
 
     ## Gauge Coordinates
     g_lon = g_lons[g]
@@ -207,11 +195,13 @@ for g in gs:
     if g_lon < 180:
         g_lon = g_lon + 360
     print('Lat: %s Lon: %s' %(g_lat,g_lon))
-    g_wl = coords['wl_var'][g]
-    g_date = coords['date_var'][g]
     
     ## Determine which tiles cover the gauge
-    passes_tiles = ['%s_%s' %(i,j) for i,j in zip(ast.literal_eval(gauge_info['pass'][0]),ast.literal_eval(gauge_info['tile'][0]))] 
+    try:
+        passes_tiles = ['%s_%s' %(i,j) for i,j in zip(ast.literal_eval(gauge_info['pass'][0]),ast.literal_eval(gauge_info['tile'][0]))] 
+    except:
+        print('points_template.csv does not contain pass, scene, and tile information. Go back and run SWOT_get_pass-scene-tile.py')
+        break
     print(passes_tiles)
     
     for product in swot_products:
@@ -225,8 +215,7 @@ for g in gs:
 
 
 
-        output1 = gauge_folder / ('%s_%s_%s_%skm_%s_%s.geojson' %(aoi,product,l2_processing,search_radius_hr,gauge_name,types))
-        print('[%s] Output=' % product,output1)        
+        output1 = output_dir / ('%s_%s_%s_%skm_%s_%s.geojson' %(aoi,product,l2_processing,search_radius_hr,gauge_name,types))
         if (os.path.isfile(output1)==False) :
             save_swot_df = {k: [] for k in variables_to_save + (['dist','unipix','avgtime','mode','cycle','pass_tile','file','avgtimestr','gauge','good'])}
     
@@ -235,6 +224,7 @@ for g in gs:
                 print('\n\n\n[%s %s %s]\tPass-Tile: %s ' %(aoi, product, gauge_name, pass_tile))
                 pass_dates = np.unique([matching_passtiles[i].split('/')[-1].split('_')[13][:8] for i in range(len(matching_passtiles))])
                 pass_dates.sort()
+                print('\n\n\n[%s %s %s]\tPass-Tile: %s --> %s dates' %(aoi, product, gauge_name, pass_tile, len(pass_dates)))
                 for date in pass_dates:
                     year = int(date[:4])
                     matches = sorted(glob.glob(str(L2HR_folder/ ('%s_*%s*%s*.nc' %(product,pass_tile,date)))))
@@ -315,8 +305,8 @@ for g in gs:
                             
                         else:
                             print('[%s %s P%s D%s]\t\t\tThere are no good SWOT data near the gauge' %(product, gauge_name, pass_tile,date))  
-                        print(len(save_swot_df['gauge']))
-                         
+                        # print(len(save_swot_df['gauge']))
+                        
                    
                                                 
             if len(save_swot_df['file'])>0:
@@ -330,19 +320,22 @@ for g in gs:
                 bad_cycles = []
                 save_swot_gdf = save_swot_gdf[~save_swot_gdf['cycle'].isin(bad_cycles)]
                 
-                watermask = gpd.read_file(gauge_folder / ('%s_water_connected_10.shp' %(aoi))).to_crs('4326').explode()
-                test = []
-                test2 = []
-                for i in range(0,len(watermask)): 
-                    # save_swot_gdf = gpd.overlay(save_swot_gdf,watermask,how='intersection')
-                    test.append(save_swot_gdf.within(watermask.iloc[i].geometry).values)
-                for i in range(0,len(save_swot_gdf)):
-                    test3 = []
-                    for j in range(0,len(watermask)):
-                        test3.append(test[j][i])
-                    test2.append(any(test3))
-                save_swot_gdf[str(gauge_folder / ('%s_water_connected_10.shp' %(aoi)))] = test2
-        
+                try:
+                    watermask = gpd.read_file(output_dir / ('%s_watermask.shp' %(aoi))).to_crs('4326').explode()
+                    test = []
+                    test2 = []
+                    for i in range(0,len(watermask)): 
+                        # save_swot_gdf = gpd.overlay(save_swot_gdf,watermask,how='intersection')
+                        test.append(save_swot_gdf.within(watermask.iloc[i].geometry).values)
+                    for i in range(0,len(save_swot_gdf)):
+                        test3 = []
+                        for j in range(0,len(watermask)):
+                            test3.append(test[j][i])
+                        test2.append(any(test3))
+                    save_swot_gdf[str(output_dir / ('%s_water_connected_10.shp' %(aoi)))] = test2
+                except:
+                    print('Water mask shapefile not available so water flag is not included. If you want to manually flag water pixels, provide a water mask file in the Examples/%s subfolder' %(aoi))
+                
                 avgtimes = pd.to_datetime(save_swot_gdf['avgtime'])#.dt.tz_localize('UTC')
                 save_swot_gdf['avgtime']=avgtimes
                 save_swot_gdf.set_index('avgtimestr',inplace=True)
@@ -359,7 +352,8 @@ for g in gs:
                     print('[%s] Output=' % product,output1)
                 else:
                     print('[%s] There are no good SWOT pixels in any passes for this product' % product)
-                
+            print('[%s] Output=' % product,output1)        
+
      
             
      
