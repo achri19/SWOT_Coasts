d7f7256c5010b7102d4dfbcab31c988ac703c3ab
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2026-05-22 11:17:08 -0700

    Update SWOT_L2_HR_Raster_ExtractNearPoint.py

--- src/SWOT_L2_HR_Raster_ExtractNearPoint.py
+++ src/SWOT_L2_HR_Raster_ExtractNearPoint.py
@@ -31,10 +31,8 @@ import sys
 
 
 ## Import custom packages 
-from swot_utils import (haversine, 
-                        add_WSE_HRL2,
-                        calculate_delh_MeantTide_minus_TideFree_geoidheights, 
-                        convert_geoid_fromMeanTide_toTideFree,
+from swot_utils import (haversine_vectorized, fix_latlon
+
                         )                            
 
 
@@ -47,6 +45,7 @@ parser.add_argument('--aoi', dest='aoi', type=str, help='If not interactive, you
 parser.add_argument('--i', dest='index_array',type = int,  help='if running job array, what index number')
 parser.add_argument('--t', dest='types',type = str,  help='profiles or points')
 parser.add_argument('--r', dest='search_radius_hr',type = float,  help='search radius in km')
+parser.add_argument('--limited', dest='limited', action="store_true",   help='Limited will only save a few attributes, other will save all attributes')
 args = parser.parse_args()
 if len(sys.argv) < 2:
     args.interactive = True
@@ -56,7 +55,7 @@ if len(sys.argv) < 2:
 ##############################################################################
 ## Set Directory
 base_dir = Path(os.path.realpath(__file__)).parent.parent
-# base_dir = Path(os.path.dirname(os.getcwd())).parent
+# base_dir = Path(os.path.dirname(os.getcwd()))
 print(base_dir)
 
 ##############################################################################
@@ -107,9 +106,9 @@ else:
 l2version = 'D' # 'C' ## Version C has shortname = 2.0, but filename = C
 l2hr_prefix = 'SWOT_L2_HR_Raster' 
 if l2version == 'C':
-    L2HR_folder = base_dir / 'Data' / (l2hr_prefix + '_2.0') 
+    L2HR_folder = base_dir / 'Data' / (l2hr_prefix + '_2.0') / aoi / 'Extracted'
 else:
-    L2HR_folder = base_dir / 'Data' / (l2hr_prefix + '_' + l2version)
+    L2HR_folder = base_dir / 'Data' / (l2hr_prefix + '_' + l2version)/ aoi / 'Extracted'
 
 l2_processing = 'P*%s*' %(l2version) #Get all version C and use only the highest number
 resolution = '100m'
@@ -117,7 +116,11 @@ resolution = '100m'
 
 ## Get L2 LR variables from reference dataset
 try:
-    ref_l2 = glob.glob(str(L2HR_folder / ('%s_%s_*%s*.nc' %(l2hr_prefix,resolution,l2_processing))))[0]
+   if args.limited:
+       ref_l2 = glob.glob(str(L2HR_folder / ('%s_%s_*_limited.nc' %(l2hr_prefix,resolution))))[0]
+   else:
+       ref_l2 = glob.glob(str(L2HR_folder / ('%s_%s_*.nc' %(l2hr_prefix,resolution))))[0]
+
 except:
     print('No matching L2 HR Raster products, make sure you downloaded files with SWOT_download_files.py first')
     sys.exit(1)
@@ -177,9 +180,9 @@ if args.index_array==None:
 else:
     gs = [gs[args.index_array]]
 
-variables_to_add = ['egm08_meantide','egm08_tidefree','wse_egm08']
-variables_to_save = variables_to_save + variables_to_add
-variables_to_remove = ['crs']#,'wind_speed_rad','rad_surface_type_flag','rad_tmb_187','rad_tmb_238','rad_tmb_340','rad_water_vapor','rad_cloud_liquid_water']
+# variables_to_add = ['egm08_meantide','egm08_tidefree','wse_egm08']
+# variables_to_save = variables_to_save + variables_to_add
+variables_to_remove = ['spatial_ref']#,'wind_speed_rad','rad_surface_type_flag','rad_tmb_187','rad_tmb_238','rad_tmb_340','rad_water_vapor','rad_cloud_liquid_water']
 variables_to_save = [x for x in variables_to_save if x not in variables_to_remove]
 # swot_products = [l2hr_prefix+ '_' + i for i in ['100m','250m'] ]
 swot_products = [l2hr_prefix+ '_' + i for i in ['100m'] ]
@@ -219,91 +222,108 @@ for g in gs:
 
         output1 = output_dir / ('%s_%s_%s_%skm_%s_%s.geojson' %(aoi,product,l2_processing.replace('*','X'),search_radius_hr,gauge_name,types))
         if (os.path.isfile(output1)==False) :
-            save_swot_df = {k: [] for k in variables_to_save + (['dist','unipix','avgtime','mode','cycle','pass_tile','file','avgtimestr','gauge','good'])}
+            save_swot_df = {k: [] for k in variables_to_save + (['x','y','x2','dist','unipix','avgtime','mode','cycle','pass_tile','file','avgtimestr','gauge'])}
     
             for pass_tile in passes_tiles:
-                matching_passtiles = glob.glob(str(L2HR_folder/ ('%s_*_%s_*%s*.nc' %(product,pass_tile,l2_processing))))
+                matching_passtiles = glob.glob(str(L2HR_folder/ ('%s_%s_C*_P%s_*%s*_*.nc' %(l2hr_prefix, resolution, pass_tile.split('_')[0], pass_tile.split('_')[1]))))
                 print('\n\n\n[%s %s %s]\tPass-Tile: %s ' %(aoi, product, gauge_name, pass_tile))
-                pass_dates = np.unique([matching_passtiles[i].split('/')[-1].split('_')[13][:8] for i in range(len(matching_passtiles))])
+                pass_dates = np.unique([matching_passtiles[i].split('/')[-1].split('_')[8] for i in range(len(matching_passtiles))])
                 pass_dates.sort()
-                print('\n\n\n[%s %s %s]\tPass-Tile: %s --> %s dates' %(aoi, product, gauge_name, pass_tile, len(pass_dates)))
+                print('\n[%s %s %s]\tPass-Tile: %s --> %s dates' %(aoi, product, gauge_name, pass_tile, len(pass_dates)))
                 for date in pass_dates:
                     year = int(date[:4])
-                    matches = sorted(glob.glob(str(L2HR_folder/ ('%s_*%s*%s*.nc' %(product,pass_tile,date)))))
+                    matches = sorted(glob.glob(str(L2HR_folder/ ('%s_%s_C*_P%s_*%s*_%s*.nc' %(l2hr_prefix, resolution, pass_tile.split('_')[0], pass_tile.split('_')[1], date)))))
                     if len(matches)>0:
                         print('\n[%s %s %s %s]' %(aoi, product, gauge_name, pass_tile))
                         print('Date: %s with %s matching files' %(date,len(matches)))
                         file = matches[-1]
                         fn = file.split('/')[-1]
-                        cycle = fn.split('_')[10]
+                        cycle = fn.split('_')[5][1:]
                         
                         
                         
-                        ds = xr.open_dataset(file)
-                        x = ds['longitude'].values.flatten()
-                        y = ds['latitude'].values.flatten()
-                        time = ds['illumination_time'].values.flatten()
+                        with xr.open_dataset(file,chunks={},engine='netcdf4') as ds:
+                            df = pd.DataFrame({'x':ds.longitude.values.flatten(),'y':ds.latitude.values.flatten()})        
+                            for variable in variables_to_save:
+                                if len(ds[variable].values.shape)==2:                
+                                    df[variable] = ds[variable].values.flatten()
+                                else:
+                                    print(variable)
+                        df = df.dropna(subset = 'x').reset_index(drop=True)
+                        df = df.dropna(subset = 'wse').reset_index(drop=True)
+                        if len(df)>0:
+                            df = fix_latlon(df)
+                            
+                        # x = ds['longitude'].values.flatten()
+                        # y = ds['latitude'].values.flatten()
+                        # time = ds['illumination_time'].values.flatten()
                         
-                        ############################################
-                        ## Geoids
-                        ds['egm08_meantide'] = ds.geoid
-                        ds['-egm08_meantide'] = -ds.geoid
+                        # ############################################
+                        # ## Geoids
+                        # ds['egm08_meantide'] = ds.geoid
+                        # ds['-egm08_meantide'] = -ds.geoid
                         
-                        ## Calculate del h, mean-tide geoid height - tide-free geoid height
-                        ## SWOT User Handbook 11.3.1, Equation 11.4 and 11.5
-                        ds = calculate_delh_MeantTide_minus_TideFree_geoidheights(ds)
+                        # ## Calculate del h, mean-tide geoid height - tide-free geoid height
+                        # ## SWOT User Handbook 11.3.1, Equation 11.4 and 11.5
+                        # ds = calculate_delh_MeantTide_minus_TideFree_geoidheights(ds)
                         
-                        ## Convert the geoid from mean-tide to tide-free
-                        geoid = 'egm08_tidefree'
-                        ds = convert_geoid_fromMeanTide_toTideFree(ds,geoid_file) 
+                        # ## Convert the geoid from mean-tide to tide-free
+                        # geoid = 'egm08_tidefree'
+                        # ds = convert_geoid_fromMeanTide_toTideFree(ds,geoid_file) 
                         
-                        ############################################
-                        ## Calculate WSE as wse - geoid
-                        final_corrections = [geoid,'-egm08_meantide']#,'delhpd']
-                        ds = add_WSE_HRL2(ds,'wse',final_corrections)    
+                        # ############################################
+                        # ## Calculate WSE as wse - geoid
+                        # final_corrections = [geoid,'-egm08_meantide']#,'delhpd']
+                        # ds = add_WSE_HRL2(ds,'wse',final_corrections)    
                         
                         ############################################
                         ## Find points within the search radius
-                        msk = []
-                        dists = []
-                        for i in range(len(y)):
-                            dist2gauge = haversine(g_lat,g_lon, y[[i]], x[[i]])
-                            if dist2gauge <= search_radius_hr:
-                                msk.append(i)        
-                                dists.append(i)
-                                
-                        ############################################
-                        ## Filter pixels by cross track distance, wse_qual, and year
-                        filters = np.where((abs(ds.cross_track.values.flatten()[msk])>=10000) & (abs(ds.cross_track.values.flatten()[msk])<=60000) 
-                                        & (ds['wse_qual'].values.flatten()[msk]==0)
-                                        & (pd.to_datetime(time[msk]).year==year),True,False)
-                        
-                        
-                                      
-                                
-                        
-                        if len(msk)>0:                        
+                        # msk = []
+                        # dists = []
+                        # for i in range(len(df)):
+                        #     dist2gauge = haversine(g_lat,g_lon, df['y'][i], df['x'][i])
+                        #     if dist2gauge <= search_radius_hr:
+                        #         msk.append(i)        
+                        #         dists.append(dist2gauge)
+                        if len(df)>0:
+                            df['dist'] = haversine_vectorized(g_lat, g_lon, df['y'], df['x2'])
+                            df = df[df['dist'] <= search_radius_hr].copy()
+                  
+                        # ############################################
+                        # ## Filter pixels by cross track distance, wse_qual, and year
+                        # filters = np.where((abs(ds.cross_track.values.flatten()[msk])>=10000) & (abs(ds.cross_track.values.flatten()[msk])<=60000) 
+                        #                 & (ds['wse_qual'].values.flatten()[msk]==0)
+                        #                 & (pd.to_datetime(time[msk]).year==year),True,False)
+                                   
+                        if len(df)>0:                        
+                            
+                            
                             # swot_time = time[(~pd.isnull(time)) & (pd.to_datetime(time).year==year)] ## some swot files have outlier dates... remove them crudely by only including ones with correct year
                             # avg_swot_time = pd.to_datetime(np.nanmean(pd.to_datetime(swot_time,unit='ns').values.astype(np.int64)),unit='ns').tz_localize('UTC') #+ datetime.timedelta(minutes=26)
                             
-                            time = pd.to_datetime(ds['illumination_time'].values.flatten()[msk],unit='ms')
-                            time = time[(~pd.isnull(time)) & (pd.to_datetime(time).year==year)] 
-                            avgtime = pd.to_datetime(np.nanmean(time.values.astype(np.int64))).tz_localize('UTC')
-                            for var in variables_to_save:
-                                save_swot_df[var].extend(ds[var].values.flatten()[msk].astype(float))
+                            # time = pd.to_datetime(ds['illumination_time'].values.flatten()[msk],unit='ms')
+                            # time = time[(~pd.isnull(time)) & (pd.to_datetime(time).year==year)] 
+                            # avgtime = pd.to_datetime(np.nanmean(time.values.astype(np.int64))).tz_localize('UTC')
+                            for var in variables_to_save + ['x','y','x2','dist']:
+                                if var in ['time','time_tai']: 
+                                    save_swot_df[var].extend(pd.to_datetime(df[var]).dt.tz_localize('UTC'))
+                                elif var == 'geometry':
+                                    save_swot_df[var].extend(df[var])
+                                else:
+                                    save_swot_df[var].extend(pd.to_numeric(df[var]))
+
                                 
-                            save_swot_df['cycle'].extend([cycle for i in range(0,len(msk))])
-                            save_swot_df['avgtime'].extend([avgtime for i in range(0,len(msk))])
-                            save_swot_df['avgtimestr'].extend([avgtime.strftime('%Y%m%d %H%M') for i in range(0,len(msk))])
-                            save_swot_df['good'].extend(filters)
+                            save_swot_df['cycle'].extend([cycle for i in range(0,len(df))])
+                            save_swot_df['avgtime'].extend([pd.to_datetime(date) for i in range(0,len(df))])
+                            save_swot_df['avgtimestr'].extend([pd.to_datetime(date).strftime('%Y%m%d %H%M') for i in range(0,len(df))])
     
-                            save_swot_df['unipix'].extend([str(abs(a)) + str(abs(b)) for a,b in zip(x[msk],y[msk])])
-                            save_swot_df['dist'].extend(dists)
-                            save_swot_df['mode'].extend([product for i in range(0,len(msk))])
+                            save_swot_df['unipix'].extend([str(abs(a)) + str(abs(b)) for a,b in  zip(df['x'],df['y'])])
+                            # save_swot_df['dist'].extend(df['dist'])
+                            save_swot_df['mode'].extend([product for i in range(0,len(df))])
     
-                            save_swot_df['file'].extend([file for i in msk])
-                            save_swot_df['pass_tile'].extend([pass_tile for i in msk])
-                            save_swot_df['gauge'].extend([gauge_name for i in msk])
+                            save_swot_df['file'].extend([file for i in range(0,len(df))])
+                            save_swot_df['pass_tile'].extend([pass_tile for i in range(0,len(df))])
+                            save_swot_df['gauge'].extend([gauge_name for i in range(0,len(df))])
                            
                             
                         else:
@@ -315,27 +335,29 @@ for g in gs:
             if len(save_swot_df['file'])>0:
                 ## Save full dataframe with SWOT variables
     
-                tmp = pd.DataFrame(np.column_stack([np.column_stack(list(save_swot_df.values()))]),columns=variables_to_save+(['dist','unipix','avgtime','mode','cycle','pass_tile','file','avgtimestr','gauge','good']))
-                save_swot_gdf = gpd.GeoDataFrame(tmp,geometry=gpd.points_from_xy(save_swot_df['longitude'],save_swot_df['latitude']))
-                save_swot_gdf.rename(columns={'x': 'longitude', 'y': 'latitude'}, inplace=True)
+                tmp = pd.DataFrame(np.column_stack([np.column_stack(list(save_swot_df.values()))]),columns=variables_to_save+(['x','y','x2','dist','unipix','avgtime','mode','cycle','pass','file','avgtimestr','gauge']))
+                save_swot_gdf = gpd.GeoDataFrame(tmp,geometry=gpd.points_from_xy(save_swot_df['x2'],save_swot_df['y']))
+                # save_swot_gdf.rename(columns={'x': 'longitude', 'y': 'latitude'}, inplace=True)
                 save_swot_gdf.crs = 'epsg:4326'
                 
                 bad_cycles = []
                 save_swot_gdf = save_swot_gdf[~save_swot_gdf['cycle'].isin(bad_cycles)]
                 
                 try:
-                    watermask = gpd.read_file(output_dir / ('%s_watermask.shp' %(aoi))).to_crs('4326').explode()
-                    test = []
-                    test2 = []
-                    for i in range(0,len(watermask)): 
-                        # save_swot_gdf = gpd.overlay(save_swot_gdf,watermask,how='intersection')
-                        test.append(save_swot_gdf.within(watermask.iloc[i].geometry).values)
-                    for i in range(0,len(save_swot_gdf)):
-                        test3 = []
-                        for j in range(0,len(watermask)):
-                            test3.append(test[j][i])
-                        test2.append(any(test3))
-                    save_swot_gdf[str(output_dir / ('%s_water_connected_10.shp' %(aoi)))] = test2
+                    watermask = gpd.read_file(output_dir / ('%s_watermask_200mbuffer.shp' %(aoi))).to_crs('4326').explode()
+                    
+                    # test = []
+                    # test2 = []
+                    # for i in range(0,len(watermask)): 
+                    #     # save_swot_gdf = gpd.overlay(save_swot_gdf,watermask,how='intersection')
+                    #     test.append(save_swot_gdf.within(watermask.iloc[i].geometry).values)
+                    # for i in range(0,len(save_swot_gdf)):
+                    #     test3 = []
+                    #     for j in range(0,len(watermask)):
+                    #         test3.append(test[j][i])
+                    #     test2.append(any(test3))
+                    joined = gpd.sjoin(save_swot_gdf, watermask, how='left', predicate='within')
+                    save_swot_gdf[str(output_dir / ('%s_water_connected_10.shp' %(aoi)))] = save_swot_gdf.index.isin(joined[joined['index_right'].notna()].index)
                 except:
                     print('Water mask shapefile not available so water flag is not included. If you want to manually flag water pixels, provide a water mask file in the Examples/%s subfolder' %(aoi))
                 
@@ -355,7 +377,7 @@ for g in gs:
                     print('[%s] Output=' % product,output1)
                 else:
                     print('[%s] There are no good SWOT pixels in any passes for this product' % product)
-            print('[%s] Output=' % product,output1)        
+            # print('[%s] Output=' % product,output1)        
 
      
             
